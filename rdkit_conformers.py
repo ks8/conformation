f4d8add5d30ae5ab501eebca19ec248dc00d071a
Kirk Swanson <swansonk1@cs.uchicago.edu>
2020-06-17 18:38:12 -0500

    First commit for conformation project

---
+++ rdkit_conformers.py
@@ -0,0 +1,45 @@
+# noinspection PyUnresolvedReferences
+from rdkit import Chem
+# noinspection PyUnresolvedReferences
+from rdkit.Chem import AllChem
+import numpy as np
+import argparse
+import os
+
+
+def conformers(args):
+	m = Chem.MolFromSmiles(args.smiles)
+	m2 = Chem.AddHs(m)
+	_ = AllChem.EmbedMultipleConfs(m2, numConfs=args.num_configs)
+	res = AllChem.MMFFOptimizeMoleculeConfs(m2, maxIters=args.max_iter)
+	rms_list = [0.0]
+	AllChem.AlignMolConformers(m2, RMSlist=rms_list)
+
+	i = 0
+	for c in m2.GetConformers():
+		np.savetxt(args.folder + "/pos-"+str(i)+".txt", c.GetPositions())
+		with open(args.folder + "/energy-rms-"+str(i)+".txt", "w") as f:
+			f.write(str(res[i][1]))
+			f.write('\n')
+			f.write(str(rms_list[i]))
+		i += 1
+
+
+def main():
+	"""
+	Parse arguments and execute file processing
+	"""
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--smiles', type=str, dest='smiles', default=None, help='Molecule SMILES string')
+	parser.add_argument('--num_configs', type=int, dest='num_configs', default=None, help='Number of conformations')
+	parser.add_argument('--folder', type=str, dest='folder', default=None, help='Folder name for saving conformations')
+	parser.add_argument('--max_iter', type=int, dest='max_iter', default=200, help='Max iter for MMFF optimization')
+	args = parser.parse_args()
+
+	os.makedirs(args.folder, exist_ok=False)
+	conformers(args)
+
+
+if __name__ == '__main__':
+	main()
+

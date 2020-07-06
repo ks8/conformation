605372cd3e5eee9a987433aef4923d5984df4c19
Kirk Swanson <swansonk1@cs.uchicago.edu>
2020-07-06 15:35:11 -0400

    dihedral option added

--- conformation/rdkit_conformers.py
+++ conformation/rdkit_conformers.py
@@ -34,13 +34,16 @@ def conformers(args: Namespace) -> None:
         pos = c.GetPositions()
         np.savetxt(os.path.join(args.out, "pos", "pos-" + str(i) + ".txt"), pos)
         dist_matrix(pos, os.path.join(args.out, "distmat", "distmat-" + str(i) + ".txt"))
-        with open(os.path.join(args.out, "properties", "energy-rms-" + str(i) + ".txt"), "w") as f:
+        with open(os.path.join(args.out, "properties", "energy-rms-dihedral-" + str(i) + ".txt"), "w") as f:
             f.write("energy: " + str(res[i][1]))
             f.write('\n')
             f.write("rms: " + str(rms_list[i]))
             f.write('\n')
-            dihedral = Chem.rdMolTransforms.GetDihedralRad(c, args.dihedral[0], args.dihedral[1], args.dihedral[2],
-                                                           args.dihedral[3])
+            if args.dihedral:
+                dihedral = Chem.rdMolTransforms.GetDihedralRad(c, args.dihedral_vals[0], args.dihedral_vals[1],
+                                                               args.dihedral_vals[2], args.dihedral_vals[3])
+            else:
+                dihedral = "nan"
             f.write("dihedral: " + str(dihedral))
         i += 1
 
@@ -55,7 +58,9 @@ def main():
     parser.add_argument('--num_configs', type=int, dest='num_configs', default=None, help='Number of conformations')
     parser.add_argument('--out', type=str, dest='out', default=None, help='Folder name for saving conformations')
     parser.add_argument('--max_iter', type=int, dest='max_iter', default=200, help='Max iter for MMFF optimization')
-    parser.add_argument('--dihedral', type=int, dest='dihedral', nargs='+', default=[2, 0, 1, 5],
+    parser.add_argument('--dihedral', action='store_true', default=False,
+                        help='Use when computing dihedral angle values')
+    parser.add_argument('--dihedral_vals', type=int, dest='dihedral_vals', nargs='+', default=[2, 0, 1, 5],
                         help='Atom IDs for dihedral')
 
     args = parser.parse_args()

dbd0a24640098f21017b08d60bfb601bd3cb19d0
Kirk Swanson <swansonk1@cs.uchicago.edu>
2020-09-27 22:06:51 -0500

    hmc fixes

--- conformation/rdkit_hmc.py
+++ conformation/rdkit_hmc.py
@@ -1,11 +1,9 @@
 """ Hamiltonian Monte Carlo conformational search using RDKit. """
 import copy
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import random
-from scipy.stats import multivariate_normal
 from typing import Tuple
 
 from rdkit import Chem
@@ -27,7 +25,6 @@ class Args(Tap):
     epsilon: float = 1  # Leapfrog step size in femtoseconds
     L: int = 10  # Number of leapfrog steps
     num_steps: int = 1  # Number of HMC steps
-    factor: float = 1
     save_dir: str  # Path to directory containing output files
 
 
@@ -35,7 +32,7 @@ class Args(Tap):
 def calc_energy_grad(mol: rdchem.Mol) -> Tuple[float, np.ndarray]:
     """
     Compute MMFF energy and gradient.
-    :return: MMFF energy and gradient.
+    :return: MMFF energy and gradient, where the energy is kcal/mol and the gradient is kcal/mol/Angstrom.
     """
     mmff_p = rdForceFieldHelpers.MMFFGetMoleculeProperties(mol)
     mmff_f = rdForceFieldHelpers.MMFFGetMoleculeForceField(mol, mmff_p)
@@ -47,18 +44,17 @@ def calc_energy_grad(mol: rdchem.Mol) -> Tuple[float, np.ndarray]:
     return energy, grad
 
 
-def calc_energy_grad_normal(q: np.ndarray, cov: np.ndarray) -> Tuple[float, np.ndarray]:
+def maxwell_boltzmann(temp: float, k_b: float, mass: float) -> np.ndarray:
     """
-    Function for computing
-    :param q: Array of points.
-    :param cov: Covariance matrix of the potential energy.
-    :return:
+    Random draw from a Maxwell-Boltzmann distribution for a given temperature and mass.
+    :param temp: Temperature (K).
+    :param k_b: Boltzmann constant (cal/K).
+    :param mass: Mass (kg).
+    :return: Random value distributed according to Maxwell-Boltzmann distribution.
     """
-    cov_inv = np.linalg.inv(cov)
-    energy = float(np.matmul(q, np.matmul(cov_inv, q)))
-    grad = np.array([0.5*(2*cov_inv[0][0]*q[0] + cov_inv[0][1]*q[1] + cov_inv[1][0]*q[1]),
-                     0.5*(cov_inv[0][1]*q[0] + cov_inv[1][0]*q[0] + 2*cov_inv[1][1]*q[1])])
-    return energy, grad
+    var = (k_b * 4.184 * temp) / mass
+    velocity = np.random.multivariate_normal(np.zeros(3), np.diag([var]*3))
+    return velocity
 
 
 def rdkit_hmc(args: Args) -> None:
@@ -67,65 +63,6 @@ def rdkit_hmc(args: Args) -> None:
     :param args: System arguments.
     :return: None.
     """
-    # current_q = np.array([-1.5, -1.55])
-    # cov = np.array([[1.0, 0.98], [0.98, 1.0]])
-    #
-    # positions = [current_q]
-    #
-    # for i in range(args.num_steps):
-    #     # Set the current position variables
-    #     q = current_q
-    #
-    #     # Generate random momentum values
-    #     if i == 0:
-    #         p = np.array([-1, 1])
-    #     else:
-    #         p = np.random.multivariate_normal(np.zeros(2), np.identity(2))
-    #     current_p = p
-    #
-    #     # Make a half-step for momentum at the beginning
-    #     _, grad_u = calc_energy_grad_normal(q, cov)
-    #     p = p - args.epsilon * grad_u/2
-    #
-    #     # Alternate full steps for position and momentum
-    #     for j in range(args.L):
-    #         # Make a full step for the position
-    #         q = q + args.epsilon * p
-    #
-    #         # Make a full step for the momentum, except at the end of the trajectory
-    #         if j != args.L - 1:
-    #             _, grad_u = calc_energy_grad_normal(q, cov)
-    #             p = p - args.epsilon * grad_u
-    #
-    #     # Make a half step for momentum at the end
-    #     _, grad_u = calc_energy_grad_normal(q, cov)
-    #     p = p - args.epsilon * grad_u/2
-    #
-    #     # Negate the momentum at the end of the trajectory to make the proposal symmetric
-    #     p = -p
-    #
-    #     # Evaluate potential and kinetic energies at start and end of the trajectory
-    #     current_u, _ = calc_energy_grad_normal(current_q, cov)
-    #     current_k = np.dot(current_p, current_p)/2.
-    #     proposed_u, _ = calc_energy_grad_normal(q, cov)
-    #     proposed_k = np.dot(p, p)/2.
-    #
-    #     prob_ratio = math.exp(current_u - proposed_u + current_k - proposed_k)
-    #     mu = random.uniform(0, 1)
-    #     if mu <= prob_ratio:
-    #         current_q = q
-    #     positions.append(current_q)
-    #
-    # positions = np.array(positions)
-    # plt.plot(positions[:, 0], positions[:, 1], 'bo', linestyle='-')
-    # plt.xlim((-2.4, 2.4))
-    # plt.ylim((-2.4, 2.4))
-    # plt.savefig("test_joint")
-    # plt.clf()
-    # plt.plot(positions[:, 0], 'bo')
-    # plt.ylim((-3.1, 3.1))
-    # plt.savefig("test_marginal")
-    # plt.clf()
     os.makedirs(args.save_dir)
 
     # Define constants
@@ -140,34 +77,46 @@ def rdkit_hmc(args: Args) -> None:
 
     print(f'Starting search: {args.smiles}')
 
-    # Generate initial conformation and minimize it
+    # Generate initial conformation
     # Here, we consider the variables of interest, q, to effectively be the atomic coordinates
     current_q = copy.deepcopy(mol)
     AllChem.EmbedMolecule(current_q, maxAttempts=args.max_attempts)
-    # AllChem.MMFFOptimizeMoleculeConfs(current_q)
     num_atoms = current_q.GetNumAtoms()
+
+    # Mass in kg
     mass = np.array([mol.GetAtomWithIdx(i).GetMass()/(1000.*avogadro) for i in range(num_atoms)])
 
+    # Add the first conformation to the list
     conformation_molecules = [current_q]
 
-    # Testing
-    test = copy.deepcopy(mol)
-    AllChem.EmbedMultipleConfs(test, numConfs=args.L*args.num_steps)
+    # ###### Testing ###############
+    # test = copy.deepcopy(mol)
+    # AllChem.EmbedMultipleConfs(test, numConfs=int(args.L/100))
+    # ######################################
 
     print(f'Running HMC steps...')
     num_accepted = 0
+    count = 0
     for _ in tqdm(range(args.num_steps)):
         # Set the current position variables
         q = copy.deepcopy(current_q)
 
-        # Generate random momentum values by sampling from the Maxwell-Boltzmann distribution
-        p = np.array([np.random.multivariate_normal(np.zeros(3), np.diag([(k_b*args.temp*4.184)/mass[i]]*3))*mass[i]
-                      for i in range(num_atoms)])
+        # Generate random momentum values by sampling velocities from the Maxwell-Boltzmann distribution
+        # Momentum is in kg * m / s
+        # Set the velocity center of mass to zero
+        v = np.array([maxwell_boltzmann(args.temp, k_b, mass[i]) for i in range(num_atoms)])
+        p = np.array([v[i]*mass[i] for i in range(num_atoms)])
+        v_cm = np.sum(p, axis=0) / sum(mass)
+        for i in range(num_atoms):
+            v[i] -= v_cm
+        p = np.array([v[i] * mass[i] for i in range(num_atoms)])
         current_p = p
 
         # Make a half-step for momentum at the beginning
+        # Note: the gradient is in kcal/mol/Angstrom, so we convert it to Newtons: 1 kg * m / s^2
         _, grad_u = calc_energy_grad(q)
-        p = p - args.epsilon * grad_u * args.factor/2
+        grad_u *= (1000.0 * 4.184 * 10e10 / avogadro)
+        p = p - args.epsilon * grad_u/2.
 
         # Alternate full steps for position and momentum
         for i in range(args.L):
@@ -175,38 +124,47 @@ def rdkit_hmc(args: Args) -> None:
             c = q.GetConformer()
             pos = c.GetPositions()
             v = np.array([p[i] / mass[i] for i in range(num_atoms)])
+            v *= 10e10  # Convert to Angstroms / s
             pos = pos + args.epsilon * v
 
             # Save updated atomic coordinates to the conformation object
             for j in range(len(pos)):
                 c.SetAtomPosition(j, Point3D(pos[j][0], pos[j][1], pos[j][2]))
 
+            # if i % 100 == 0:
+            #     # Testing ###################
+            #     c = test.GetConformers()[count]
+            #     for j in range(len(pos)):
+            #         c.SetAtomPosition(j, Point3D(pos[j][0], pos[j][1], pos[j][2]))
+            #     count += 1
+            #     #############################
+
             # Make a full step for the momentum, except at the end of the trajectory
             if i != args.L - 1:
                 energy, grad_u = calc_energy_grad(q)
-                p = p - args.epsilon * grad_u * args.factor
-                print(energy)
-            else:
-                # Testing
-                c = test.GetConformers()[i]
-                for j in range(len(pos)):
-                    c.SetAtomPosition(j, Point3D(pos[j][0], pos[j][1], pos[j][2]))
+                grad_u *= (1000.0 * 4.184 * 10e10 / avogadro)
+                p = p - args.epsilon * grad_u
 
         # Make a half step for momentum at the end
         _, grad_u = calc_energy_grad(q)
-        p = p - args.epsilon * grad_u * args.factor/2
+        grad_u *= (1000.0 * 4.184 * 10e10 / avogadro)
+        p = p - args.epsilon * grad_u/2.
+
+        # print(rdmolfiles.MolToPDBBlock(test), file=open("traj.pdb", "w+"))
 
         # Negate the momentum at the end of the trajectory to make the proposal symmetric
         p *= -1.0
 
         # Evaluate potential and kinetic energies at start and end of the trajectory
+        # Energies are in Joules
         current_u, _ = calc_energy_grad(current_q)
         current_u *= (1000.0 * 4.184 / avogadro)
-        current_k = sum(np.array([np.dot(current_p[i], current_p[i]) / (2. * mass[i]) for i in range(num_atoms)]))
+        current_k = sum([((np.linalg.norm(current_p[i]))**2 / (2. * mass[i])) for i in
+                         range(num_atoms)])
         proposed_u, _ = calc_energy_grad(q)
-        print("proposed: " + str(proposed_u))
         proposed_u *= (1000.0 * 4.184 / avogadro)
-        proposed_k = sum(np.array([np.dot(p[i], p[i]) / (2. * mass[i]) for i in range(num_atoms)]))
+        proposed_k = sum([((np.linalg.norm(p[i]))**2 / (2. * mass[i])) for i in
+                          range(num_atoms)])
 
         prob_ratio = math.exp((current_u - proposed_u + current_k - proposed_k) / (k_b*args.temp*4.184))
         mu = random.uniform(0, 1)
@@ -214,8 +172,6 @@ def rdkit_hmc(args: Args) -> None:
             current_q = q
             conformation_molecules.append(current_q)
             num_accepted += 1
-
-    print(rdmolfiles.MolToPDBBlock(test), file=open("traj.pdb", "w+"))
 
     print(f'Number of conformations identified: {len(conformation_molecules)}')
     print(f'% Moves accepted: {float(num_accepted) / float(args.num_steps) * 100.0}')
@@ -256,5 +212,3 @@ def rdkit_hmc(args: Args) -> None:
             unique_conformer_indices.append(i)
 
     print(f'Number of unique post minimization conformations identified: {len(unique_conformer_indices)}')
-
-

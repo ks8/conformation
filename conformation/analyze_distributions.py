5b1eb97f9fb64143865c527fed9a61c9ee188f8a
Kirk Swanson <swansonk1@cs.uchicago.edu>
2021-02-07 22:23:58 -0600

    relying just on seaborn

--- conformation/analyze_distributions.py
+++ conformation/analyze_distributions.py
@@ -22,11 +22,8 @@ class Args(Tap):
     """
     data_path: str  # Path to RDKit binary file containing conformations
     num_energy_decimals: int = 3  # Number of energy decimals used for computing empirical minimized energy probability
-    subsample_frequency: int = 1  # Frequency at which to compute sample information
     weights: bool = False  # Whether or not to weight histograms by empirical Boltzmann probability
     temp: float = 300.0  # Temperature for Boltzmann weighting (weights = True)
-    pairwise_joint_histograms_figsize: int = 10  # Height and width for figsize
-    pairwise_joint_histograms_dpi: int = 200  # dpi for figure
     save_dir: str  # Path to directory containing output files
 
 
@@ -89,11 +86,10 @@ def analyze_distributions(args: Args) -> None:
 
     angles = [[] for _ in range(len(rotatable_bonds))]
     for i in tqdm(range(mol.GetNumConformers())):
-        if i % args.subsample_frequency == 0:
-            c = mol.GetConformer(i)
-            for j in range(len(rotatable_bonds)):
-                angles[j].append(rdMolTransforms.GetDihedralRad(c, atom_indices[j][0], atom_indices[j][1],
-                                                                atom_indices[j][2], atom_indices[j][3]))
+        c = mol.GetConformer(i)
+        for j in range(len(rotatable_bonds)):
+            angles[j].append(rdMolTransforms.GetDihedralRad(c, atom_indices[j][0], atom_indices[j][1],
+                                                            atom_indices[j][2], atom_indices[j][3]))
 
     if args.weights:
         weights = compute_energy_weights(mol, k_b, temp, avogadro)
@@ -102,57 +98,18 @@ def analyze_distributions(args: Args) -> None:
 
     print("Computing pairwise joint torsion histograms via Seaborn")
     df = pd.DataFrame(np.array(angles).transpose())
+    column_names = dict()
+    for i, bond in enumerate(rotatable_bonds):
+        atom_0 = mol.GetAtomWithIdx(bond[0]).GetSymbol()
+        atom_1 = mol.GetAtomWithIdx(bond[1]).GetSymbol()
+        column_names[i] = f'{bond[0]}-{bond[1]} | {atom_0}-{atom_1}'
+    df = df.rename(columns=column_names)
     g = sns.PairGrid(df)
     g.set(ylim=(-math.pi - 1., math.pi + 1.), xlim=(-math.pi - 1., math.pi + 1.))
     g.map_upper(sns.histplot, bins=list(np.arange(-math.pi - 1., math.pi + 1., 0.1)), weights=weights)
     g.map_lower(sns.kdeplot, fill=True, weights=weights, bw_adjust=0.25)
     g.map_diag(sns.histplot, bins=list(np.arange(-math.pi - 1., math.pi + 1., 0.1)), weights=weights)
     g.savefig(os.path.join(args.save_dir, "pairwise_joint_histograms_seaborn.png"))
-    plt.close()
-
-    for i, bond in enumerate(rotatable_bonds):
-        fig, ax = plt.subplots()
-        if args.weights:
-            # noinspection PyUnboundLocalVariable
-            df = np.concatenate((np.array(angles[i])[:, np.newaxis], weights[:, np.newaxis]), axis=1)
-            df = pd.DataFrame(df)
-            df = df.rename(columns={0: 'Angle', 1: 'Probability'})
-            sns.histplot(df, x='Angle', ax=ax, bins=list(np.arange(-math.pi - 1., math.pi + 1., 0.1)),
-                         weights='Probability')
-        else:
-            sns.histplot(angles[i], ax=ax, bins=np.arange(-math.pi - 1., math.pi + 1., 0.1))
-        ax.set_xlabel("Angle (radians)")
-        ax.set_ylabel("Count")
-        atom_0 = mol.GetAtomWithIdx(bond[0]).GetSymbol()
-        atom_1 = mol.GetAtomWithIdx(bond[1]).GetSymbol()
-        ax.figure.savefig(os.path.join(args.save_dir,
-                                       f'rotatable-bond-{bond[0]}-{bond[1]}-{atom_0}-{atom_1}-distribution.png'))
-        plt.clf()
-        plt.close()
-
-    fig = plt.figure(constrained_layout=False, dpi=args.pairwise_joint_histograms_dpi,
-                     figsize=[args.pairwise_joint_histograms_figsize, args.pairwise_joint_histograms_figsize])
-    gs = fig.add_gridspec(len(rotatable_bonds), len(rotatable_bonds))
-    for i, bond_i in enumerate(rotatable_bonds):
-        for j, bond_j in enumerate(rotatable_bonds):
-            ax = fig.add_subplot(gs[i, j])
-            ax.hist2d(angles[j], angles[i], density=True, bins=np.arange(-math.pi - 1., math.pi + 1., 0.1),
-                      cmap='viridis', weights=weights)
-            if i == j:
-                ax.spines['top'].set_visible(True)
-                ax.spines['bottom'].set_visible(True)
-                ax.spines['left'].set_visible(True)
-                ax.spines['right'].set_visible(True)
-                ax.spines['top'].set_linewidth(1.0)
-                ax.spines['top'].set_color('red')
-                ax.spines['bottom'].set_linewidth(1.0)
-                ax.spines['bottom'].set_color('red')
-                ax.spines['left'].set_linewidth(1.0)
-                ax.spines['left'].set_color('red')
-                ax.spines['right'].set_linewidth(1.0)
-                ax.spines['right'].set_color('red')
-            ax.set(xticks=[], yticks=[])
-    plt.savefig(os.path.join(args.save_dir, "pairwise_joint_histograms.png"))
     plt.close()
 
     print("Computing minimized energies...")
